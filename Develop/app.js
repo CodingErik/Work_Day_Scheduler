2a253e5a234e2de3602140bcb2f1a5303cc56f56
Erik De Luna <codingerik1.gmail.com>
2020-05-19 22:45:34 -0500

    finished work hour if else statements moving on to button

--- Develop/app.js
+++ Develop/app.js
@@ -1,32 +1,57 @@
 // setting up a moment object variable 
 let m = moment();
 
-// current day, month, day, year, time
-$('#currentDay').text(m.format("dddd, MMMM Do YYYY, h:mm a"));
+// current day, month, day, year, time time running the seconds
+$("#currentDay").text(moment().format("MMMM Do YYYY hh:mm:ss A"));
+
+setInterval(function () {
+    $("#currentDay").text(moment().format("MMMM Do YYYY hh:mm:ss A"));
+}, 1000);
 
 
-let toDos =  ['','','','','','','','','','']
+let toDos = ['', '', '', '', '', '', '', '', '', '']
 // plugin in the times for the planner
 
 // check for the current time
-    // set the class according to the current time of the day
-    
-    
+// set the class according to the current time of the day
+
+
+
+
 for (let i = 0; i < toDos.length; i++) {
-    
-    $('#time' + i ).text(moment({ hour: 8 + i }).format('hA'));
+    let workHours = moment({ hour: 8 + i })
+    let currentHour = moment().hour();
+    $('#time' + i).text(workHours.format('hA'));
+
+    // test variable to make sure the if conditional if else statements are working 
+    // let test = moment({ hour: 12}).hour();
+
+
+
+    if (workHours.hour() < currentHour) {
+        $('#toDos' + i).addClass('past');
+        $('#toDos' + i).attr("placeholder", "time has passed already");
+    } else if (workHours.hour() > currentHour) {
+        $('#toDos' + i).addClass('future');
+        $('#toDos' + i).attr("placeholder", "plan now!");
+    } else {
+        $('#toDos' + i).addClass('present');
+    }
+
+
+    // save button is able to save to the local storage when clicked
+    $('.saveBtn').on('click', function (event) {
+
+        
+        console.log($('#toDos9'+i));
+
+        
+       
 
 
 
 
-
-
-
-
-// save button is able to save to the local storage when clicked
-$('.saveBtn').on('click', function(event) {
-    console.log(event.target.value);
-})
+    })
 
 };
 
